0fd77f2ca945dd4618849d92f52b70f5d0766d4c
Igor Shishkin <me@teran.ru>
2018-04-06 20:35:00 +0300

    Add vgo stuff, update tests to use testify
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- cache/sql_test.go
+++ cache/sql_test.go
@@ -13,21 +13,19 @@ type SQLCacheTestSuite struct {
 	db *sql.DB
 }
 
-func (s *SQLCacheTestSuite) TestMySQLCache(t *testing.T) {
+func (s *SQLCacheTestSuite) TestMySQLCache() {
 	dataSample := []byte("test-byte")
 	c, _ := NewSQLCache(s.db)
 
-	if err := c.Put(context.Background(), "test-data", dataSample); err != nil {
-		t.Errorf("Error puting data to cache: %s", err)
-	}
+	err := c.Put(context.Background(), "test-data", dataSample)
+	s.Require().NoError(err)
 
-	if _, err := c.Get(context.Background(), "test-data"); err != nil {
-		t.Errorf("Error obtaining data from cache: %s", err)
-	}
+	data, err := c.Get(context.Background(), "test-data")
+	s.Require().NoError(err)
+	s.Require().NotNil(data)
 
-	if err := c.Delete(context.Background(), "test-data"); err != nil {
-		t.Errorf("Error deleting data from cache: %s", err)
-	}
+	err = c.Delete(context.Background(), "test-data")
+	s.Require().NoError(err)
 }
 
 func (s *SQLCacheTestSuite) SetupTest() {
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module "github.com/teran/svcproxy"
 
 require (
+	"github.com/stretchr/testify" v1.2.1
 	"golang.org/x/crypto" v0.0.0-20180403160946-b2aa35443fbc
 	"golang.org/x/sys" v0.0.0-20180406135729-3b87a42e500a
 )
