f57c317f96e862e30f7cf514f6ad49949695befe
Igor Shishkin <me@teran.ru>
2018-04-06 20:23:22 +0300

    Rename MySQLCache to SQLCache, Update tests
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- src/svcproxy/cache/sql_test.go
+++ src/svcproxy/cache/sql_test.go
@@ -2,12 +2,20 @@ package cache
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestMySQLCache(t *testing.T) {
+type SQLCacheTestSuite struct {
+	suite.Suite
+	db *sql.DB
+}
+
+func (s *SQLCacheTestSuite) TestMySQLCache(t *testing.T) {
 	dataSample := []byte("test-byte")
-	c := MySQLCache{}
+	c, _ := NewSQLCache(s.db)
 
 	if err := c.Put(context.Background(), "test-data", dataSample); err != nil {
 		t.Errorf("Error puting data to cache: %s", err)
@@ -20,4 +28,12 @@ func TestMySQLCache(t *testing.T) {
 	if err := c.Delete(context.Background(), "test-data"); err != nil {
 		t.Errorf("Error deleting data from cache: %s", err)
 	}
+}
+
+func (s *SQLCacheTestSuite) SetupTest() {
+	s.db = nil
+}
+
+func TestMySQLCacheTestSuite(t *testing.T) {
+	suite.Run(t, new(SQLCacheTestSuite))
 }
