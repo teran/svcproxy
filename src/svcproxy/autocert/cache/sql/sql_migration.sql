64294e1c977b527b48a99dafb32d5344630ce161
Igor Shishkin <me@teran.ru>
2018-04-08 21:30:24 +0300

    Fix migration to be compatible with PostgreSQL
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- src/svcproxy/autocert/cache/sql/sql_migration.sql
+++ src/svcproxy/autocert/cache/sql/sql_migration.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `autocert_cache` (
-  `cache_key` VARCHAR(255) NOT NULL PRIMARY KEY,
-  `cache_value` TEXT NOT NULL
+CREATE TABLE autocert_cache (
+  cache_key VARCHAR(255) NOT NULL PRIMARY KEY,
+  cache_value TEXT NOT NULL
 );
