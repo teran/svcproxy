06245067632838099403aa61456951c62e7df253
Igor Shishkin <me@teran.ru>
2018-04-14 15:21:05 +0300

    Move sql driver initialization to dedicated function
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- src/svcproxy/autocert/cache/cache.go
+++ src/svcproxy/autocert/cache/cache.go
@@ -6,42 +6,46 @@ import (
 
 	"golang.org/x/crypto/acme/autocert"
 
-	sqlcache "svcproxy/autocert/cache/sql"
-
 	// MySQL driver
 	_ "github.com/go-sql-driver/mysql"
 
 	// PostgreSQL driver
 	_ "github.com/lib/pq"
+
+	sqlcache "svcproxy/autocert/cache/sql"
 )
 
 // NewCacheFactory returns Cache instance
 func NewCacheFactory(backend string, options map[string]string) (autocert.Cache, error) {
 	switch backend {
 	case "sql":
-		driver, ok := options["driver"]
-		if !ok {
-			return nil, fmt.Errorf("No driver specified")
-		}
-		dsn, ok := options["dsn"]
-		if !ok {
-			return nil, fmt.Errorf("dsn option to backend is required")
-		}
-		db, err := sql.Open(driver, dsn)
-		if err != nil {
-			return nil, fmt.Errorf("Error establishing database connection: %s", err)
-		}
-		if e := db.Ping(); e != nil {
-			return nil, fmt.Errorf("Error contacting database: %s", e)
-		}
-
-		encryptionKey, ok := options["encryptionKey"]
-		if !ok || encryptionKey == "" {
-			return nil, fmt.Errorf("encryptionKey option to backend is required and cannot be empty")
-		}
-
-		return sqlcache.NewCache(db, []byte(encryptionKey))
+		return newSQLCacheBackend(options)
 	}
 
 	return nil, fmt.Errorf("Unknown backend specified")
+}
+
+func newSQLCacheBackend(options map[string]string) (autocert.Cache, error) {
+	driver, ok := options["driver"]
+	if !ok {
+		return nil, fmt.Errorf("No driver specified")
+	}
+	dsn, ok := options["dsn"]
+	if !ok {
+		return nil, fmt.Errorf("dsn option to backend is required")
+	}
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Error establishing database connection: %s", err)
+	}
+	if e := db.Ping(); e != nil {
+		return nil, fmt.Errorf("Error contacting database: %s", e)
+	}
+
+	encryptionKey, ok := options["encryptionKey"]
+	if !ok || encryptionKey == "" {
+		return nil, fmt.Errorf("encryptionKey option to backend is required and cannot be empty")
+	}
+
+	return sqlcache.NewCache(db, []byte(encryptionKey))
 }
