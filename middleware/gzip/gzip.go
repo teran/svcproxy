26e77fcae909c4eebbb67760ec3a1cfeb43b074a
Igor Shishkin <me@teran.ru>
2018-09-20 14:56:52 +0300

    Add ability to set compression level
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- middleware/gzip/gzip.go
+++ middleware/gzip/gzip.go
@@ -3,6 +3,7 @@ package gzip
 import (
 	"compress/gzip"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 
@@ -21,7 +22,9 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 }
 
 // Gzip middleware type
-type Gzip struct{}
+type Gzip struct {
+	Level int
+}
 
 // NewMiddleware returns new Gzip middleware instance
 func NewMiddleware() *Gzip {
@@ -29,7 +32,22 @@ func NewMiddleware() *Gzip {
 }
 
 // SetOptions sets passed options for middleware at startup time(i.e. Chaining procedure)
-func (g *Gzip) SetOptions(_ map[string]interface{}) {}
+func (g *Gzip) SetOptions(opts map[string]interface{}) {
+	levelField, ok := opts["level"]
+	if ok {
+		level, ok := levelField.(int)
+		if !ok {
+			log.Fatal("gzip middleware: error verifying gzip level: probably wrong type, must be integer")
+		}
+
+		if level < gzip.HuffmanOnly || level > gzip.BestCompression {
+			log.Fatalf("gzip middleware: invalid compression level: %d. Must be between %d and %d", level, gzip.HuffmanOnly, gzip.BestCompression)
+		}
+
+		g.Level = level
+	}
+	g.Level = gzip.DefaultCompression
+}
 
 // Middleware wraps handler into GZIP content encoding
 func (g *Gzip) Middleware(next http.Handler) http.Handler {
@@ -41,7 +59,10 @@ func (g *Gzip) Middleware(next http.Handler) http.Handler {
 
 		w.Header().Set("Content-Encoding", "gzip")
 
-		gz := gzip.NewWriter(w)
+		gz, err := gzip.NewWriterLevel(w, g.Level)
+		if err != nil {
+			log.Fatalf("Unexpected error while initializing gzip writer: %s", err)
+		}
 
 		defer gz.Close()
 
